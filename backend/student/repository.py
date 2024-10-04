548e6e00f16137cfb7b4497ab10d378df8df23a9
JSANT16 <jardm12@hotmail.com>
2024-10-04 16:03:28 -0600

    EFEX: Added type hints

--- backend/student/repository.py
+++ backend/student/repository.py
@@ -3,20 +3,20 @@ from .models import Student
 
 class StudentRepository:
     @staticmethod
-    def create_student(data):
+    def create_student(data: Student) -> Student:
         student = Student.objects.create(**data)
         return student
 
     @staticmethod
-    def get_all_students():
+    def get_all_students() -> list[Student]:
         return Student.objects.all()
 
     @staticmethod
-    def get_student_by_id(student_id):
+    def get_student_by_id(student_id: int):
         return Student.objects.filter(id=student_id).first()
     
     @staticmethod
-    def update_student(student_id, data):
+    def update_student(student_id, data: Student) -> Student:
         student = Student.objects.filter(id=student_id).first()
         if student:
             for key, value in data.items():
--- backend/student/use_cases.py
+++ backend/student/use_cases.py
@@ -1,22 +1,23 @@
+from .models import Student
 from .repository import StudentRepository
 from .serializers import StudentSerializer
 
 class CreateStudentUseCase:
-    def execute(self, data):
+    def execute(self, data:Student) -> Student:
         student = StudentRepository.create_student(data)
         return StudentSerializer(student).data
 
 class GetStudentsUseCase:
-    def execute(self):
+    def execute(self) -> list[Student]:
         students = StudentRepository.get_all_students()
         return StudentSerializer(students, many=True).data
 
 class GetStudentByIdUseCase:
-    def execute(self, student_id):
+    def execute(self, student_id:int) -> Student | None:
         student = StudentRepository.get_student_by_id(student_id)
         return StudentSerializer(student).data if student else None
     
 class UpdateStudentUseCase:
-    def execute(self, student_id, data):
+    def execute(self, student_id:int, data:Student) -> Student | None:
         student = StudentRepository.update_student(student_id, data)
         return StudentSerializer(student).data if student else None
